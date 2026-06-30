427d9dd8a94d843e2bda6f345488379f69fb0ed2
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-06-30 20:12:47 +0530

    starting the Ticketing-system

---
+++ frontend/test.js
@@ -0,0 +1,41 @@
+let value = `-@import "tailwindcss";
+-
+-:root {
+-  --background: #ffffff;
+-  --foreground: #171717;
+-}
+-
+-@theme inline {
+-  --color-background: var(--background);
+-  --color-foreground: var(--foreground);
+-  --font-sans: var(--font-geist-sans);
+-  --font-mono: var(--font-geist-mono);
+-}
+-
+-@media (prefers-color-scheme: dark) {
+-  :root {
+-    --background: #0a0a0a;
+-    --foreground: #ededed;
+-  }
+-}
+-
+-body {
+-  background: var(--background);
+-  color: var(--foreground);
+-  font-family: Arial, Helvetica, sans-serif;
+-}`
+
+let newString = ``
+
+
+for (let x = 1; x < value.length; x++){
+
+    if (value[x-1] == "\n"){
+        continue
+    }
+    
+    newString += value[x]
+}
+
+
+console.log(newString)
